b1871c576b704ce5f2dbdb8d88562f5b490fef8e
kerzan <mikael.verdu@gmail.com>
2022-04-11 18:45:13 +0200

    add draw function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 let nbRows = 0;
 let nbCols = 0;
 const grid = [];
@@ -7,7 +8,6 @@ const cellSize = 10;
 const fillTheGrid = () => {
   nbRows = Math.floor(canvas.height / cellSize);
   nbCols = Math.floor(canvas.width / cellSize);
-
   for (let l = 0; l < nbRows; l++) {
     const row = [];
     for (let c = 0; c < nbCols; c++) {
@@ -23,4 +23,16 @@ const init = () => {
   fillTheGrid();
 };
 
+const draw = () => {
+  for (let l = 0; l < grid.length; l++) {
+    for (let c = 0; c < grid[l].length; c++) {
+      if (grid[l][c] === "wall") {
+        ctx.fillStyle = "black";
+        ctx.fillRect(c * cellSize, l * cellSize, cellSize, cellSize);
+      }
+    }
+  }
+};
+
 init();
+draw();
